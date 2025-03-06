e1e043aab20a2528e5c322397e62956410048ee5
Kristina-tina <tina.krstn2220@gmail.com>
2025-03-06 19:48:07 +0200

    Add files via upload

---
+++ if else/if.js
@@ -0,0 +1,82 @@
+"use strict";
+
+//if-else, switch, function//
+
+//1//
+//let num1 = "prompt('First number')";
+//num1 = Number(num1);
+//let num2 = prompt('Second number');
+//num2 = Number(num2);
+//let sum = num1 + num2;
+
+//if (sum /= 5) {
+//alert('Сума кратна 5');
+//} else {
+// alert('Сума не кратна 5');
+//}
+
+//2//
+/*const calculateArea = side => side * side;
+let side = 4;
+let area = calculateArea(side);
+console.log("Площадь квадрата:", area);*/
+
+//3// 
+
+//const userMenu = Number(prompt("1 - Чай,\n 2 - Кава,\n 3 - Сік,\n 4 - Вода"));
+
+//switch (userMenu) {
+//case 1: {
+//alert('Чай');
+//break;
+//}
+//case 2:{
+//alert('Кава');
+//break;
+//}
+//case 3: {
+//alert('Сік');
+//break;
+//}
+//case 4: {
+//alert('Вода');
+//}
+//break;
+//default: alert('Такого напою у нас нема');
+//break;
+// }
+
+//4//
+
+//let userAmount = +prompt('Your amount?');
+//let amount1 = 500; 
+//let amount2 = 800;
+//let resultString = userAmount >= amount1 ? '3%' : (userAmount >= amount2 ? '5%' : 'Нет скидки') ; 
+//alert('Your discount ' + resultString); 
+
+
+//5// 
+
+/*const calc = function(num1,num2) {
+  if (num1 % num2 === 0){
+  alert('true');
+  } else{
+    alert('false');
+  }
+  return a % b;
+};
+console.log ('calc(num1%num2)') ; 
+
+let num1 = Number(prompt('Number1'));
+let num2 = Number(prompt('Number2'));
+
+let isDivisible = function(num1, num2) {
+  return num1 % num2 === 0;
+}
+let result = isDivisible(num1, num2);
+alert(result); */
+
+
+
+
+
